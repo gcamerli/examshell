d555e808e3c6c0f93f797a4a9c91cbb9f88c3e8c
Gius. Camerlingo <g.camerlingo@protonmail.ch>
2016-10-14 12:14:43 +0200

    Create and add ft_strcmp

---
+++ level2/ft_strcmp/ft_strcmp.c
@@ -0,0 +1,14 @@
+int	ft_strcmp(char *s1, char *s2)
+{
+	int i;
+	
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	if (s1[i] > s2[i])
+		return (1);
+	else if (s1[i] < s2[i])
+		return (-1);
+	else
+		return (0);
+}
