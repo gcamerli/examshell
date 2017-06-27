4b9df6a5f378cad29c8a70ab54512efc638a1880
Gius. Camerlingo <gcamerli@protonmail.com>
2017-06-27 13:41:20 +0200

    Update level2

--- level2/alpha_mirror/alpha_mirror.c
+++ level2/alpha_mirror/alpha_mirror.c
@@ -7,27 +7,38 @@ void	ft_putchar(char c)
 
 char	ft_alpha_mirror(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
 			ft_putchar('M' - (str[i] - 'N'));
+		}
 		else if (str[i] >= 'a' && str[i] <= 'z')
+		{
 			ft_putchar('m' - (str[i] - 'n'));
+		}
 		else
+		{
 			ft_putchar(str[i]);
+		}
 		i++;
 	}
 	ft_putchar('\n');
+	return (*str);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
-		ft_alpha_mirror(argv[1]);	
+	{
+		ft_alpha_mirror(argv[1]);
+	}
 	else
+	{
 		ft_putchar('\n');
+	}
 	return (0);
 }
